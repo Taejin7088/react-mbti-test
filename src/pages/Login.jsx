209d84af25dbb42be6e2d3c457cd15b4ab6f8b73
taejin7088 <gog70888@gmail.com>
2025-02-21 18:38:16 +0900

    feat : 로그인 ui 완성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <></>;
 };
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <section className='flex flex-col bg-white w-[600px] mr-auto ml-auto mt-10 items-center'>
+      <h1 className='text-[40px] p-[20px]'>회원가입</h1>
+
+      <div className='bg-gray-100 p-4 rounded-md'>
+        <form
+          className='flex flex-col w-[500px] p-4 rounded-md gap-7'
+          action='submit'
+        >
+          <input
+            type='text'
+            placeholder='아이디'
+            className='bg-white border rounded-md p-5'
+          />
+          <input
+            type='password'
+            placeholder='비밀번호'
+            className='bg-white border rounded-md p-5'
+          />
+          <input
+            type='password'
+            placeholder='닉네임'
+            className='bg-white border rounded-md p-5'
+          />
+          <button className='bg-red-500 border rounded-md p-5 text-white'>
+            회원가입
+          </button>
+        </form>
+      </div>
+
+      <div className='pb-[20px] pt-[10px]'>
+        이미계정이 있으신가요?
+        <Link className='text-red-500 ml-3 font-bold' to={'/login'}>
+          로그인
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
