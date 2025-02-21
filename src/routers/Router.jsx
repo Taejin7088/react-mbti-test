efbba941668859f60cef589ca516d2cdd8599fc1
taejin7088 <gog70888@gmail.com>
2025-02-21 13:02:09 +0900

    refator : 퍼블릭프리베이트 구분 코드 수정

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
 import Login from '../pages/Login';
@@ -8,41 +8,37 @@ import Results from '../pages/Results';
 
 const isLogin = false;
 
-const privateRoute = () => {
+const PrivateRoute = () => {
   //해당 페이지에 접근하는데 로그인 안했으면 로그인페이지로 보냄
-  let path = '';
   if (!isLogin) {
-    path = <Navigate to='/Login' />;
+    return <Navigate to='/Login' />;
   }
-  return (
-    <>
-      <Route path='/profile' element={path || <Profile />} />
-      <Route path='/test' element={path || <Test />} />
-      <Route path='/results' element={path || <Results />} />
-    </>
-  );
+  return <Outlet />;
 };
 
-const publicRoute = () => {
+const PublicRoute = () => {
   //해당 페이지에 접근하는데 로그인 했으면 마이페이지로 보냄
-  let path = '';
   if (isLogin) {
-    path = <Navigate to='/profile' replace />;
+    return <Navigate to='/profile' replace />;
   }
-  return (
-    <>
-      <Route path='/login' element={path || <Login />} />
-      <Route path='/signup' element={path || <Signup />} />
-    </>
-  );
+  return <Outlet />;
 };
 
 const Router = () => {
   return (
     <Routes element={<Home />}>
       <Route path='/' element={<Home />} />
-      {privateRoute()}
-      {publicRoute()}
+
+      <Route element={<PublicRoute />}>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Route>
+
+      <Route element={<PrivateRoute />}>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/test' element={<Test />} />
+        <Route path='/results' element={<Results />} />
+      </Route>
     </Routes>
   );
 };
