d3e3e87eecbb507726038e3974581f213da805cc
taejin7088 <gog70888@gmail.com>
2025-02-25 11:19:54 +0900

    db.json

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,2 @@
 export const AUTH_API_URL = 'https://www.nbcamp-react-auth.link';
-export const MBTI_API_URL = 'http://localhost:4000/testResults';
+export const MBTI_API_URL = 'https://ivory-phantom-snipe.glitch.me';
