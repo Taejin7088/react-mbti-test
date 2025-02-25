d45986e1d3e7d5920a89813ab6c6a0bbc96a8e66
taejin7088 <gog70888@gmail.com>
2025-02-25 14:17:03 +0900

    Refresh .gitignore

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,7 @@
 export const AUTH_API_URL = 'https://www.nbcamp-react-auth.link';
-export const MBTI_API_URL = 'https://yellow-terrific-court.glitch.me';
+
+//글리치배포되면 url변경 예정
+export const MBTI_API_URL = 'http://localhost:4000/testResults';
+
+export const DEPLOYMENT_URL =
+  'https://react-mbti-test-a85864p4w-taejin7088s-projects.vercel.app/';
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react';
-
 import TestForm from '../components/TestForm';
 import { Link } from 'react-router-dom';
 import { mbtiDescriptions } from '../data/mbtiDescriptions';
+import { useSelector } from 'react-redux';
 
 const Test = () => {
   //테스트결과를 제출하지 않아서 값이 비어 있으면 테스트보여주고
   //테스트를 완료해서 MBTI 결과가 있으면 결과를 보여줌
   const [userMbti, setUserMbti] = useState('');
+  const { nickname } = useSelector((state) => state.auth);
+
+  //글리치 배포되면 링크 바꾸기 > DEPLOYMENT_URL
+  const link = `localhost:5173/`;
+  const resultLink = `${link}mbti-share?nickname=${nickname}&mbti=${userMbti}`;
+  const copyResultLinkHandler = async () => {
+    await navigator.clipboard.writeText(resultLink);
+    alert('링크가 복사되었습니다.');
+  };
+
+  const faceBookShareHandler = () => {
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      resultLink
+    )}`;
+    console.log('facebookUrl', facebookUrl);
+    window.open(facebookUrl, '_blank', 'width=600,height=400');
+  };
 
   return userMbti ? (
     <>
@@ -19,11 +36,23 @@ const Test = () => {
           <div className='py-7 mb:text-3xl'>{mbtiDescriptions[userMbti]}</div>
         </div>
         <Link
-          className='bg-red-500 border rounded-md md:w-[50%] w-[80%] p-5 m-5 mb-[50px] text-white flex justify-center items-center'
+          className='bg-red-500 border rounded-md md:w-[50%] w-[80%] p-5 m-5 text-white flex justify-center items-center'
           to='/results'
         >
           <button className=''>결과 페이지로 이동하기</button>
         </Link>
+        <button
+          onClick={copyResultLinkHandler}
+          className='bg-red-500 border rounded-md md:w-[50%] w-[80%] p-5 mb-5 text-white flex justify-center items-center'
+        >
+          테스트 결과 공유링크 복사하기
+        </button>
+        <button
+          onClick={faceBookShareHandler}
+          className='bg-red-500 border rounded-md md:w-[50%] w-[80%] p-5 mb-[50px] text-white flex justify-center items-center'
+        >
+          테스트 결과 공유링크 복사하기
+        </button>
       </section>
     </>
   ) : (
