2919efbca06754ddf0d7e188b1295c5a6254a2fd
taejin7088 <gog70888@gmail.com>
2025-02-25 11:25:10 +0900

    링크수정

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,2 @@
 export const AUTH_API_URL = 'https://www.nbcamp-react-auth.link';
-export const MBTI_API_URL = 'https://ivory-phantom-snipe.glitch.me';
+export const MBTI_API_URL = 'https://yellow-terrific-court.glitch.me';
